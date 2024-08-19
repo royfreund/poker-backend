6aa83e29a1354065b24dd5a391aa9b08f1209b47
royfreund <royfreund14@gmail.com>
2024-08-19 15:43:46 +0300

    Player type

--- src/player/dto/create-player.dto.ts
+++ src/player/dto/create-player.dto.ts
@@ -1,1 +1,8 @@
-export class CreatePlayerDto {}
+export class CreatePlayerDto {
+  firstName: string;
+  lastName: string;
+  gamesPlayed: number;
+  gamesProfited: number;
+  gamesLost: number;
+  money: number;
+}
--- src/player/entities/player.entity.ts
+++ src/player/entities/player.entity.ts
@@ -1,1 +1,5 @@
-export class Player {}
+import { CreatePlayerDto } from '../dto/create-player.dto';
+
+export class Player extends CreatePlayerDto {
+  id: string;
+}
