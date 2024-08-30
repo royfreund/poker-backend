6c36ef5a2d9fbd85db770214d3965c00d80ed36b
royfreund <royfreund14@gmail.com>
2024-08-30 17:51:13 +0300

    init games module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,7 @@
 import { Module } from '@nestjs/common';
 import { PlayersModule } from './player/players.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { GamesModule } from './games/games.module';
 
 @Module({
   imports: [
@@ -15,6 +16,7 @@ import { TypeOrmModule } from '@nestjs/typeorm';
       synchronize: true,
     }),
     PlayersModule,
+    GamesModule,
   ],
 })
 export class AppModule {}
